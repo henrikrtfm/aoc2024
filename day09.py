ed446756aeeae046c9001d1ca4f8feb2be9d821e
Henrik Edsparr <henrik.edsparr@gmail.com>
2024-12-11 07:35:28 +0100

    Solution for Day 9

---
+++ day09.py
@@ -0,0 +1,102 @@
+from utils.readinput import read_input
+
+
+def parse_day09_input():
+    #day09_input = '2333133121414131402'
+    day09_input = read_input('input/day09.txt')
+    block_representation = []
+    file_id = 0
+    for i, char in enumerate(day09_input):
+        size = int(char)
+        if i % 2 == 0:
+            block_representation.extend([file_id] * size)
+            file_id += 1
+        else:
+            block_representation.extend(['.'] * size)
+
+    return block_representation
+
+
+def compact_disc(disk):
+    last_free_space_idx = 0
+
+    for idx in range(len(disk) - 1, -1, -1):
+        if disk[idx] == '.':
+            continue
+
+        while last_free_space_idx < len(disk) and disk[last_free_space_idx] != '.':
+            last_free_space_idx += 1
+
+        if last_free_space_idx < idx:
+            disk[last_free_space_idx], disk[idx] = disk[idx], '.'
+            last_free_space_idx += 1
+
+    return disk
+
+
+def find_free_space_block(disk, size):
+    start_idx = 0
+    while start_idx < len(disk):
+        if disk[start_idx] == '.':
+            free_space_size = 0
+            while start_idx + free_space_size < len(disk) and disk[start_idx + free_space_size] == '.':
+                free_space_size += 1
+            if free_space_size >= size:
+                return start_idx, free_space_size
+            start_idx += free_space_size
+        else:
+            start_idx += 1
+    return None, None
+
+
+def compact_disc_blocks(disk):
+    """Compact the disk by moving entire file blocks if possible."""
+    compacted = disk[:]
+    idx = len(compacted) - 1  # Start from the end of the disk
+
+    while idx >= 0:
+        if compacted[idx] == '.':  # Skip free space
+            idx -= 1
+            continue
+
+        file_id = compacted[idx]
+        block_end = idx
+        block_size = 0
+
+        # Find the start and size of the file block
+        while block_end >= 0 and compacted[block_end] == file_id:
+            block_size += 1
+            block_end -= 1
+        block_start = block_end + 1
+
+        # Find a suitable free space block
+        free_start, free_size = find_free_space_block(compacted, block_size)
+        if free_start is not None and free_start < block_start:
+            # Move the entire file block
+            compacted[free_start:free_start + block_size] = [file_id] * block_size
+            compacted[block_start:block_start + block_size] = ['.'] * block_size
+
+        # Update the index to skip over the processed block
+        idx = block_end  # Move to the next unprocessed block
+
+    return compacted
+
+
+def calculate_checksum(disk):
+    return sum(idx * int(block) for idx, block in enumerate(disk) if block != '.')
+
+
+def solve_part_1(block_representation):
+    compacted_disc = compact_disc(block_representation)
+    return calculate_checksum(compacted_disc)
+
+
+def solve_part_2(block_representation):
+    compacted_disc = compact_disc_blocks(block_representation)
+    return calculate_checksum(compacted_disc)
+
+
+if __name__ == '__main__':
+    parsed_input = parse_day09_input()
+    print("Answer to part 1: {}".format(solve_part_1(parsed_input)))
+    print("Answer to part 2: {}".format(solve_part_2(parsed_input)))
---
+++ day10.py
@@ -0,0 +1,81 @@
+from utils.readinput import read_input_as_list
+
+DIRECTIONS = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+
+
+def parse_day10_input():
+    #day10_input = read_input_as_list('input/day10_sample.txt')
+    day10_input = read_input_as_list('input/day10.txt')
+    topographic_map = {}
+    trailheads = []
+    for row in range(len(day10_input)):
+        for col in range(len(day10_input[0])):
+            height = int(day10_input[row][col])
+            topographic_map[(row, col)] = height
+            if height == 0:
+                trailheads.append((row, col))
+
+    return topographic_map, trailheads
+
+
+def get_neighbors(coordinate, topographic_map):
+    neighbors = [(coordinate[0] + dx, coordinate[1] + dy) for dx, dy in DIRECTIONS]
+    valid_coordinates = [neighbor for neighbor in neighbors if neighbor in topographic_map]
+    return valid_coordinates
+
+
+def is_safe(this, next, topographic_map):
+    if topographic_map[next] - topographic_map[this] == 1:
+        return True
+    else:
+        return False
+
+
+def find_trails(trailhead, topographic_map):
+    visited = set()
+    stack = [trailhead]
+    trails = 0
+
+    while stack:
+        current = stack.pop()
+        visited.add(current)
+        if topographic_map[current] == 9:
+            trails += 1
+        neighbors = get_neighbors(current, topographic_map)
+        for neighbor in neighbors:
+            if neighbor not in visited and is_safe(current, neighbor, topographic_map):
+                stack.append(neighbor)
+    return trails
+
+
+def find_trail_rating(trailhead, topographic_map):
+    visited = set()
+    stack = [trailhead]
+    rating = 0
+
+    while stack:
+        current = stack.pop()
+        visited.add(current)
+        neighbors = get_neighbors(current, topographic_map)
+        if topographic_map[current] == 9:
+            rating += 1
+        for neighbor in neighbors:
+            if is_safe(current, neighbor, topographic_map):
+                stack.append(neighbor)
+    return rating
+
+
+def solve_part_1(topographic_map, trailheads):
+    trails = [find_trails(trailhead, topographic_map) for trailhead in trailheads]
+    return sum(trails)
+
+
+def solve_part_2(topographic_map, trailheads):
+    ratings = [find_trail_rating(trailhead, topographic_map) for trailhead in trailheads]
+    return sum(ratings)
+
+
+if __name__ == '__main__':
+    topographic_map, trailheads = parse_day10_input()
+    print("Answer to part 1: {}".format(solve_part_1(topographic_map, trailheads)))
+    print("Answer to part 2: {}".format(solve_part_2(topographic_map, trailheads)))
